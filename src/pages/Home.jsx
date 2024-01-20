fe8a9b1de67fbc038505211299d4680cb22bdf57
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-01-20 16:20:45 +0200

    Create Home Page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Navigation from "../components/Navigation"
+
+
+const Home = () => {
+  return (
+    <main className="w-full absolute min-h-full overflow-x-hidden">
+        <Navigation />
+    </main>
+  )
+}
+
+export default Home
