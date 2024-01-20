6ffe04d491cf7c3b50a1a8ef620bfa3da7c23b6e
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-01-20 16:20:34 +0200

    Implement React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
 
   return (
-    <>
-      <h1 className="text-2xl">Hello world!</h1>
-    </>
+    <Routes>
+      <Route exact path="/" element={<Home />}/>
+    </Routes>
   )
 }
 
