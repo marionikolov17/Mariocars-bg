7ebf210d15120712ea69c1f7a062e934af5ec548
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-01-20 16:20:59 +0200

    Create Navigation Component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+
+
+const Navigation = () => {
+  return (
+    <div className="w-full h-16 flex overflow-hidden bg-sky-700">
+        <div className="w-[15%] h-full flex items-center justify-center">
+            <img src="/images/logo.png" alt="Car Tire Logo" className="w-12 h-12" />
+            <h2 className="ml-3 text-white text-2xl font-bold">MarioCars</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Navigation
