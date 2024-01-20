099651ca2472394d5432fa0a248d6aae8ea622e6
Mario Nikolov <133797684+marionikolov17@users.noreply.github.com>
2024-01-20 16:42:54 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Navigation from "../components/Navigation"
 
 const Home = () => {
   return (
-    <main className="w-full absolute min-h-full overflow-x-hidden">
+    <main className="w-full absolute min-h-full overflow-x-hidden font-roboto">
         <Navigation />
     </main>
   )
